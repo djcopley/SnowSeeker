89a6f3c15e486b04b17d833c0fc1fe36814843da
Daniel Copley <djcopley@users.noreply.github.com>
2023-02-11 18:02:12 -0500

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Daniel Copley on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
